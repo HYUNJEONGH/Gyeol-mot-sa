ad66cc25f638d2a1057fcabef37280c19ffa6ff8
HYUNJEONGH <hwahgyj@outlook.com>
2017-02-26 17:13:54 +0900

    Initial commit

---
+++ app/src/main/java/com/teamgms/gms/gms/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.teamgms.gms.gms.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.teamgms.gms.gms.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
